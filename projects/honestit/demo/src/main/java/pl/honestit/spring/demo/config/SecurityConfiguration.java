30e6bcbc7e44ef2e5b916ffd7f1e4f967c4973f4
honestit <michal.kupisinski@gmail.com>
2020-01-19 04:53:15 +0100

    M2.6: Dodanie beana do szyfrowania hasła

--- projects/honestit/demo/src/main/java/pl/honestit/spring/demo/config/SecurityConfiguration.java
+++ projects/honestit/demo/src/main/java/pl/honestit/spring/demo/config/SecurityConfiguration.java
@@ -1,11 +1,19 @@
 package pl.honestit.spring.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- projects/honestit/demo/src/main/java/pl/honestit/spring/demo/controllers/RegistrationController.java
+++ projects/honestit/demo/src/main/java/pl/honestit/spring/demo/controllers/RegistrationController.java
@@ -2,6 +2,7 @@ package pl.honestit.spring.demo.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,11 +15,13 @@ import pl.honestit.spring.demo.model.repositories.UserRepository;
 @Slf4j
 public class RegistrationController {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationController(UserRepository userRepository) {
+    public RegistrationController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping
@@ -30,7 +33,7 @@ public class RegistrationController {
     public String processRegistrationPage(String username, String password, String firstName, String lastName) {
         User user = User.builder()
                 .username(username)
-                .password(password)
+                .password(passwordEncoder.encode(password))
                 .firstName(firstName)
                 .lastName(lastName)
                 .active(false)
