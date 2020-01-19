3debfef1ee267bf51630b8073c1ce9bd605c2d8a
honestit <michal.kupisinski@gmail.com>
2020-01-19 04:40:33 +0100

    M2.2: Szkielet kontrolera do rejestracji

---
+++ projects/honestit/demo/src/main/java/pl/honestit/spring/demo/controllers/RegistrationController.java
@@ -0,0 +1,21 @@
+package pl.honestit.spring.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+
+    @GetMapping
+    public String prepareRegistrationPage() {
+        return "";
+    }
+
+    @PostMapping
+    public String processRegistrationPage() {
+        return "";
+    }
+}
