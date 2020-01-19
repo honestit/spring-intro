79134ecca449c993555a42121491fedfefcc6fe4
honestit <michal.kupisinski@gmail.com>
2020-01-19 03:38:40 +0100

    M1.1: Wygenerowany projekt ze strony spring.io

---
+++ projects/honestit/demo/src/test/java/pl/honestit/spring/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.honestit.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
