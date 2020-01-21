c4700ee68bb781aa9b719cc1d3b95879b1ed8dee
honestit <michal.kupisinski@gmail.com>
2020-01-21 06:14:05 +0100

    M3.3: Kontroler dla ścieżki głównej (bez obsługi)

---
+++ projects/honestit/demo/src/main/java/pl/honestit/spring/demo/controllers/HomePageController.java
@@ -0,0 +1,9 @@
+package pl.honestit.spring.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomePageController {
+}
