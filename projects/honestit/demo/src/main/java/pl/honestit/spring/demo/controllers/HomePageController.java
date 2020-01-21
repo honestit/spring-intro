1fe992e0dd7587bd1c68ddb44f9eed979345b9c0
honestit <michal.kupisinski@gmail.com>
2020-01-21 06:15:55 +0100

    M3.4: Uzupełniona obsługa w kontrolerze strony głównej dla metody GET

--- projects/honestit/demo/src/main/java/pl/honestit/spring/demo/controllers/HomePageController.java
+++ projects/honestit/demo/src/main/java/pl/honestit/spring/demo/controllers/HomePageController.java
@@ -1,9 +1,30 @@
 package pl.honestit.spring.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.honestit.spring.demo.model.domain.Advert;
+import pl.honestit.spring.demo.model.repositories.AdvertRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomePageController {
+
+    private final AdvertRepository advertRepository;
+
+    @Autowired
+    public HomePageController(AdvertRepository advertRepository) {
+        this.advertRepository = advertRepository;
+    }
+
+    @GetMapping
+    public String prepareHomePage(Model model) {
+        List<Advert> adverts = advertRepository.findAllByOrderByPostedDesc();
+        model.addAttribute("adverts", adverts);
+        return "/WEB-INF/views/home-page.jsp";
+    }
 }
--- projects/honestit/demo/src/main/java/pl/honestit/spring/demo/model/repositories/AdvertRepository.java
+++ projects/honestit/demo/src/main/java/pl/honestit/spring/demo/model/repositories/AdvertRepository.java
@@ -3,5 +3,8 @@ package pl.honestit.spring.demo.model.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.honestit.spring.demo.model.domain.Advert;
 
+import java.util.List;
+
 public interface AdvertRepository extends JpaRepository<Advert, Long> {
+    List<Advert> findAllByOrderByPostedDesc();
 }
