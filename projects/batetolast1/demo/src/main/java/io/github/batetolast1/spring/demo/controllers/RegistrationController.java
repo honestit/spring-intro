f2f0f38f458784e75075b9a16ae1c8c57e654dbb
batetolast1 <batetolast1@gmail.com>
2020-09-23 16:14:42 +0200

    added Registration Controller class

---
+++ projects/batetolast1/demo/src/main/java/io/github/batetolast1/spring/demo/controllers/RegistrationController.java
@@ -0,0 +1,23 @@
+package io.github.batetolast1.spring.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // tworzy komponent typu kontroler Spring MVC (odpowiednik servletu)
+@RequestMapping("/register") // mapuje kontroler na wskazaną ścieżkę
+public class RegistrationController {
+
+    @GetMapping // obsługuje żądanie typu GET
+    public String prepareRegistrationPage() {
+        // TODO
+        return ""; // metoda ma zwracać identyfikator widoku (JSP)
+    }
+
+    @PostMapping // obsługuje żądanie typu POST
+    public String processRegistrationPage() {
+        // TODO
+        return "";
+    }
+}
