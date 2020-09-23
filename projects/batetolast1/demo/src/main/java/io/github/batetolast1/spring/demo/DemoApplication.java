d33675666cb82aeaa6f4eb9b81e922f319c2ac6d
batetolast1 <batetolast1@gmail.com>
2020-09-23 15:47:39 +0200

    added description to code written in module 1

--- projects/batetolast1/demo/src/main/java/io/github/batetolast1/spring/demo/DemoApplication.java
+++ projects/batetolast1/demo/src/main/java/io/github/batetolast1/spring/demo/DemoApplication.java
@@ -3,11 +3,12 @@ package io.github.batetolast1.spring.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // adnotacja, która pozwala na domyślną konfigurację aplikacji
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args); // statyczna metoda odpowiedzialna za uruchomienie aplikacji
 	}
 
+	// nowe klasy muszą być tworzone w podpakietach pakietu demo (poniżej tej klasy)
 }
