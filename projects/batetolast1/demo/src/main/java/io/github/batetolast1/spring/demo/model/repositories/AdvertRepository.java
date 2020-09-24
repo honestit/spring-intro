9381e5b804c98020216be4e89f02ee1cae459dad
batetolast1 <batetolast1@gmail.com>
2020-09-24 18:29:58 +0200

    added method to fetch all Adverts sorted by date descending

--- projects/batetolast1/demo/src/main/java/io/github/batetolast1/spring/demo/model/repositories/AdvertRepository.java
+++ projects/batetolast1/demo/src/main/java/io/github/batetolast1/spring/demo/model/repositories/AdvertRepository.java
@@ -3,5 +3,8 @@ package io.github.batetolast1.spring.demo.model.repositories;
 import io.github.batetolast1.spring.demo.model.domain.Advert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AdvertRepository extends JpaRepository<Advert, Long> {
+    List<Advert> findAllByOrderByPostedDesc();
 }
