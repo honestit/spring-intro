867709a91a72f22c7f80b9b510a2a83cfeb81e7e
batetolast1 <batetolast1@gmail.com>
2020-09-24 15:30:39 +0200

    added logging feature using user credentials stored in data source (MySQL database)

--- projects/batetolast1/demo/src/main/java/io/github/batetolast1/spring/demo/config/SecurityConfiguration.java
+++ projects/batetolast1/demo/src/main/java/io/github/batetolast1/spring/demo/config/SecurityConfiguration.java
@@ -1,17 +1,31 @@
 package io.github.batetolast1.spring.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-// dodajemy własną konfigurację bezpieczeństwa, która pozwala wejść na stronę rejestracji bez logowania
+import javax.sql.DataSource;
+
+// dodajemy własną konfigurację bezpieczeństwa
 
 @Configuration // klasa konfiguracji musi posiadać adnotację @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter { // dodatkowo musi rozszerzać klasę WebSecurityConfigurerAdapter
 
+    // wstrzykujemy obiekt javax.sql.DataSource skonfigurowany w pliku application.properties
+    private final DataSource dataSource;
+
+    // wstrzyknięcie przez konstruktor
+    @Autowired
+    public SecurityConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    // umożliwienie wejścia na stronę rejestracji bez logowania
     @Override
     protected void configure(HttpSecurity security) throws Exception { // nadpisując metodę configure() podajemy konfigurację bezpieczeństwa
         security.authorizeRequests()
@@ -44,5 +58,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter { // dod
         // zwracamy obiekt klasy DelegatingPasswordEncoder, który automatycznie dostosowuje odpowiednią implementację algorytmu do zapisanego hasła
         // domyślnie Spring korzysta z algorytmu bcrypt dostarczanego przez klasę BCryptPasswordEncoder
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // dodanie własnej implementacji zasad autentykacji użytkownika - zapytania SQL i technologia JDBC
+    @Override
+    protected void configure(AuthenticationManagerBuilder authBuilder) throws Exception {
+        authBuilder.jdbcAuthentication()
+                .dataSource(dataSource) // określenie źródła danych
+                .passwordEncoder(passwordEncoder()) // określenie mechanizmu szyfrowania hasła
+                .usersByUsernameQuery("SELECT username, password, active FROM users WHERE username = ?") // zapytanie SQL zwracające nazwę użytkownika, hasło użytkownika i flagę aktywności na podstawie nazwy użytkownika
+                .authoritiesByUsernameQuery("SELECT username, 'ROLE_USER' FROM users WHERE username = ?"); // zapytanie SQL zwracające zestaw par nazwa użytkownika-rola użytkownika na podstawie nazwy użytkownika
+
+        // ostatnie zapytanie domyślnie przypisuje rolę użytkownikowi - nie ma takiego pola w bazie danych
+        // TODO add user roles support: https://www.baeldung.com/role-and-privilege-for-spring-security-registration
     }
 }
