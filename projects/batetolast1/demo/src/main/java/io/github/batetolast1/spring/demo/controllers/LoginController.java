533c976a267ab1ac885889d4f61430bbdd8028e3
batetolast1 <batetolast1@gmail.com>
2020-09-24 16:26:26 +0200

    Created login controller

---
+++ projects/batetolast1/demo/src/main/java/io/github/batetolast1/spring/demo/controllers/LoginController.java
@@ -0,0 +1,43 @@
+package io.github.batetolast1.spring.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    /*
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public LoginController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    */
+
+    @GetMapping
+    public String prepareLoginPage() {
+        return "/WEB-INF/views/login-page.jsp";
+    }
+
+    /*
+    // @PostMapping nie jest wymagane!
+    @PostMapping
+    public String processLoginPage(String username,
+                                   String password) {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) throw new UsernameNotFoundException(username);
+
+        if (passwordEncoder.matches(password, user.getPassword())) {
+            return "redirect:index.html";
+        } else {
+            return "redirect:/login";
+        }
+    }
+    */
+}
