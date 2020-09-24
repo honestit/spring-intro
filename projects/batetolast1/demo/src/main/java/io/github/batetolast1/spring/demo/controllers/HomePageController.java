cb4d577e6577888c7a7125795820a349d029b37c
batetolast1 <batetolast1@gmail.com>
2020-09-24 18:24:29 +0200

    created controller for home page

---
+++ projects/batetolast1/demo/src/main/java/io/github/batetolast1/spring/demo/controllers/HomePageController.java
@@ -0,0 +1,9 @@
+package io.github.batetolast1.spring.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomePageController {
+}
