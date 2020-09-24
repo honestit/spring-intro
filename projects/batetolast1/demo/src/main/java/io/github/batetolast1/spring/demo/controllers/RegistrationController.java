fb33a2be36804687173988dad7a1fec507f4237d
batetolast1 <batetolast1@gmail.com>
2020-09-24 14:30:24 +0200

    injected UserRepository interface object to controller, implemented saving new user to database

--- projects/batetolast1/demo/src/main/java/io/github/batetolast1/spring/demo/controllers/RegistrationController.java
+++ projects/batetolast1/demo/src/main/java/io/github/batetolast1/spring/demo/controllers/RegistrationController.java
@@ -1,5 +1,8 @@
 package io.github.batetolast1.spring.demo.controllers;
 
+import io.github.batetolast1.spring.demo.model.domain.User;
+import io.github.batetolast1.spring.demo.model.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,14 +12,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/register") // mapuje kontroler na wskazaną ścieżkę
 public class RegistrationController {
 
+    private final UserRepository userRepository;
+
+    @Autowired // wstrzykujemy obiekt interfesju UserRepository do kontrolera (wzorzec Dependency Injection)
+    public RegistrationController(UserRepository userRepository) { // korzystamy z zalecanej opcji wstrzyknięcia przez konstruktor
+        this.userRepository = userRepository;
+    }
+
+    // inne dostępne opcje dla wstrzyknięć
+    // użycie adnotacji @Autowired bezpośrednio nad polem reprezentującym obiekt, który chcemy wstrzyknąć (niezalecane)
+    // użycie adnotacji @Autowired bezpośrednio nad metodą typu set dla pola, które chcemy wstrzyknąć
+
     @GetMapping // obsługuje żądanie typu GET
     public String prepareRegistrationPage() {
         return "/WEB-INF/views/registration-form.jsp"; // metoda ma zwracać identyfikator widoku (JSP)
     }
 
     @PostMapping // obsługuje żądanie typu POST
-    public String processRegistrationPage() {
-        // TODO
-        return "";
+    public String processRegistrationPage(String username, // wartości są tworzone automatycznie na podstawie danych z formularza przesyłanych w żądaniu
+                                          String password, // bardziej precyzyjne mapowania możemy osiągnąć, stosując adnotację @RequestParam przy każdym z parametrów
+                                          String firstName, // np. gdy dany parametr ma być nieobowiązkowy albo gdy parametr żądania nazywa się inaczej niż parametr metody
+                                          String lastName) {
+        User user = User.builder()
+                .username(username)
+                .password(password)
+                .firstName(firstName)
+                .lastName(lastName)
+                .active(false)
+                .build();
+
+        user = userRepository.save(user);
+
+        return "redirect:/index.html";
     }
 }
