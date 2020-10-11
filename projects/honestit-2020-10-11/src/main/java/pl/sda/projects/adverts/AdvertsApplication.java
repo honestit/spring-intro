9d6cde0e84ab32c82c8eb9ff6c0ad583307d5220
honestit <michal.kupisinski@gmail.com>
2020-10-11 10:40:29 +0200

    New project impl

---
+++ projects/honestit-2020-10-11/src/main/java/pl/sda/projects/adverts/AdvertsApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.projects.adverts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertsApplication.class, args);
+	}
+
+}
